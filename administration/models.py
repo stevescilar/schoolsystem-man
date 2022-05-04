b7ba8d4ec7b3fd3079afb6b0b6d187b08b87a8a7
stevescilar <stevemuambi@gmail.com>
2022-05-04 17:01:15 +0300

    updated

---
+++ administration/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# student model
+# parents model
+# classmodule
+# subject model
+# exam module
+# Fee Module
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    sirname = models.CharField(max_length=50)
+    
